6ba47e0bb9fe6bd89b71162063af971686cae53e
dkustov <dvkustov@gmail.com>
2016-12-10 21:52:47 +0300

    знак '='

--- TaskNode/Parsers/AssignCharAcceptor.cs
+++ TaskNode/Parsers/AssignCharAcceptor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,20 @@ namespace TaskNode.Parsers
 {
     public class AssignCharAcceptor:ICharAction
     {
+        public Node CurrentNode { get; set; }
+        private bool SignDetected = false;
+
         public bool Accept(char ch)
         {
-            throw new NotImplementedException();
+            if (!SignDetected && !((char.IsWhiteSpace(ch) || char.IsControl(ch)) && ch != '='))
+                throw new Exception($"ожидался знак '=' , а получен знак '{ch}'");
+
+            SignDetected = SignDetected || ch == '=';
+
+            return ch == '=' || char.IsWhiteSpace(ch) || char.IsControl(ch);
+
+
+
         }
     }
 }
